1d993d74899ef3c4b53111d5553c34a3ef44fd4b
webappsagency <agcastro.py@yahoo.com>
2019-09-21 12:55:25 -0300

    db setup

--- musicplayce/settings/production.py
+++ musicplayce/settings/production.py
@@ -6,10 +6,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['.centraldobrasil.club']
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'djongo',
-    #     'NAME': 'musicplayce',
-    # } mongodb://mp-db:d115e68fc9a342d500b20919fe58955f@dokku-mongo-mp-db:27017/mp_db
+
     'default': {
         'default': dj_database_url.parse(os.environ.get('MONGO_URL')),
     }
